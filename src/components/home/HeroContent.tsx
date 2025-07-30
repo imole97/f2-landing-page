35092ec34268faf68df79b3ec8e95edc15b71a62
Imoleayo Adebanjo <imoleayoadebanjo@Imoleayo.local>
2025-07-30 19:26:47 +0100

    changes to hero

--- src/components/home/HeroContent.tsx
+++ src/components/home/HeroContent.tsx
@@ -7,14 +7,14 @@ const HeroContent = () => {
     <>
       <Container className="z-20 top-[152px] lg:top-[240px] right-[2.5%] left-[2.5%] absolute ">
         <div className="flex flex-col">
-          <div className="bg-[#FFBB00] w-fit rounded-full px-4 py-[6px] text-xl text-[#0D102F] font-medium lg:py-2 lg:text-[14px]">
+          <div className="bg-[#FFBB00] w-fit rounded-full px-4 py-[6px] sm:text-xl text-[#0D102F] font-medium lg:py-2 ">
             Venture Studio
           </div>
-          <div className="max-w-[822px] font-bold mt-4 lg:leading-24 text-[40px] lg:text-[83px]">
-            <span>
+          <div className="max-w-[850px] font-bold mt-4 lg:leading-24 text-[40px] lg:text-[83px]">
+         
               Building and Backing Africa&#39;s Next AI-First Startups from 0 -
               1 in partnership with brazen entrepreneurs
-            </span>
+      
           </div>
         </div>
       </Container>
@@ -23,6 +23,7 @@ const HeroContent = () => {
           alt="vsf-trusted"
           className="aspect-[142/80] w-auto h-[clamp(80px,4vw,80px)] rounded-[8px]"
           src="/images/vsf-trusted.png"
+          imgClassname="rounded-[8px]"
         />
       </div>
     </>
