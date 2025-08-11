ee7ed46def5924ac85131bafbdf3894435e3ae43
Imoleayo Adebanjo <89104863+imole97@users.noreply.github.com>
2025-08-11 20:01:58 +0100

    Update page.tsx

--- src/app/(main)/download/page.tsx
+++ src/app/(main)/download/page.tsx
@@ -8,7 +8,7 @@ const Download = () => {
      useEffect(() => {
     const downloadPdf = () => {
       const link = document.createElement('a');
-      link.href = `https://drive.google.com/uc?export=download&id=14ysso4ImRn4nbhI74fzoVxuvhrn9YVIV`;
+      link.href = `https://drive.google.com/uc?export=download&id=1m2TFLj2smaGvs8RpA-jcAVfucaD-oGFh`;
       link.download = 'file.pdf'; // Specify the file name for the downloaded file
       link.click();
     };
